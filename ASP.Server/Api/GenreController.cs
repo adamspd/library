c95ada44893b5215209cf62c8bd3af175a7213d1
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2023-02-15 23:27:04 +0100

    added genre in api

--- ASP.Server/Api/GenreController.cs
+++ ASP.Server/Api/GenreController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ASP.Server.Database;
 using Microsoft.AspNetCore.Mvc;
 using ASP.Server.Model;
-
+using ASP.Server.Data;
 
 namespace ASP.Server.Api
 {
@@ -25,15 +26,49 @@ namespace ASP.Server.Api
             return libraryDbContext.Genre.ToList();
         }
 
-        public ActionResult<Genre> GetGenreById(int genreId)
+        public async Task<ActionResult<Genre>> GetGenreById(int genreId)
         {
-            var queryable = libraryDbContext.Genre.AsQueryable();
-            if (queryable.Select(genre => genre.Id == genreId) == null)
+            var genre = await libraryDbContext.Genre
+                .Include(g => g.Books)
+                .FirstOrDefaultAsync(genre => genre.Id == genreId);
+            if (genre == null)
             {
                 return NotFound();
             }
-            return queryable.FirstOrDefault(genre => genre.Id == genreId);
+            return genre;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<GenreDto>> Create([FromBody] GenreDto createGenreDto)
+        {
+            // Create a new Genre object with the specified name
+            var genre = new Genre { Name = createGenreDto.Name };
+
+            // Add the new genre to the database
+            libraryDbContext.Genre.Add(genre);
+            await libraryDbContext.SaveChangesAsync();
+
+            // Return a GenreDto object for the newly created genre
+            var genreDto = new GenreDto { Name = genre.Name };
+            return CreatedAtAction(nameof(GetGenreById), new { genreId = genre.Id }, genre);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, GenreDto updatedGenre)
+        {
+            var genre = await libraryDbContext.Genre.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            genre.Name = updatedGenre.Name;
+            await libraryDbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // Should I write the delete genre function ? If so, what do I do with the books in where that was the genre ? remove it ?
     }
 }
 
