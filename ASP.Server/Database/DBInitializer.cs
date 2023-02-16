c6b9f3fc51bace74a650c59c29ac6b368585b6c1
Judicael99 <nshimijudi@gmail.com>
2023-02-16 10:10:45 +0100

    BookController,GenreController,Initializer, deleteBook

--- ASP.Server/Database/DBInitializer.cs
+++ ASP.Server/Database/DBInitializer.cs
@@ -19,11 +19,11 @@ namespace ASP.Server.Database
 
             Genre SF, Classic, Romance, Thriller, Programming;
             bookDbContext.Genre.AddRange(
-                SF = new Genre(),
-                Classic = new Genre(),
-                Romance = new Genre(),
-                Thriller = new Genre(),
-                Programming = new Genre()
+                SF = new Genre() { Name = "SF" },
+                Classic = new Genre() { Name = "Classic" },
+                Romance = new Genre() { Name = "Romance" },
+                Thriller = new Genre() { Name = "Thriller" },
+                Programming = new Genre() { Name= "Programming" }
             ) ;
             bookDbContext.SaveChanges();
 
@@ -32,11 +32,32 @@ namespace ASP.Server.Database
             bookDbContext.Books.AddRange(
                 new Book()
                 {
-                    Author = "Adams", Title="How to code in c#", Price=19.99, Content="Ipsum lorem dolor", Genres = new List<Genre> { Programming }
+                    Author = "Adams", Title="How to code in c#", Price=19.99F, Content="Ipsum lorem dolor", Genres = new List<Genre> { Programming }
                 },
-                new Book(),
-                new Book(),
                 new Book()
+                {
+                    Author = "Bella",
+                    Title = "How to code in Obectif-c",
+                    Price = 39.99F,
+                    Content = "Ipsum lorem dolor",
+                    Genres = new List<Genre> { Classic }
+                },
+                new Book()
+                {
+                    Author = "carole",
+                    Title = "How to code in Java",
+                    Price = 38.99F,
+                    Content = "Ipsum lorem dolor",
+                    Genres = new List<Genre> { Romance }
+                },
+                new Book()
+                {
+                    Author = "damien",
+                    Title = "How to code in phyton",
+                    Price = 45.99F,
+                    Content = "Ipsum lorem dolor",
+                    Genres = new List<Genre> { Thriller }
+                }
             );
             // Vous pouvez initialiser la BDD ici
 
