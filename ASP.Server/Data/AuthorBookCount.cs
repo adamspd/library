7c1c0bc09789beb3013efd3bd39d2e26793311dd
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2023-02-20 17:34:09 +0100

    Afficher des filtres dans la liste des livres pour filtrer par autheurs / genres Une page affichants les statistiques sur : le nombre de libres total disponible, le nombre de livres par autheur, Le nombre maximum, minmum, median et moyen de mots d'un livre.

---
+++ ASP.Server/Data/AuthorBookCount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ASP.Server.Data
+{
+    public class AuthorBookCount
+    {
+        public string Author { get; set; }
+        public int BookCount { get; set; }
+    }
+}
+
