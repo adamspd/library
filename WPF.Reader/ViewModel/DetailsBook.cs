608d5d4fff8d44e83398c8fe0f5453eb9aeab29d
Judicael99 <nshimijudi@gmail.com>
2023-02-20 16:06:17 +0100

    design

--- WPF.Reader/ViewModel/DetailsBook.cs
+++ WPF.Reader/ViewModel/DetailsBook.cs
@@ -10,26 +10,6 @@ namespace WPF.Reader.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand ReadCommand { get; init; }
-
-
-
-        public Book SelectedBook;
-
-        public Book SelectedLine
-        {
-            get { return SelectedBook; }
-            set
-            {
-                if (value != null)
-                {
-                    SelectedBook = value;
-                    Ioc.Default.GetService<INavigationService>().Navigate<ReadBook>(SelectedBook);
-                }
-
-            }
-        }
-
-        // n'oublier pas faire de faire le binding dans DetailsBook.xaml !!!!
         public Book CurrentBook { get; init; }
 
         public DetailsBook(Book book)
@@ -37,17 +17,15 @@ namespace WPF.Reader.ViewModel
             CurrentBook = book;
             ReadCommand = new RelayCommand(x => {
 
-                /* A vous de définir la commande */
                 Ioc.Default.GetRequiredService<INavigationService>().Navigate<ReadBook>(CurrentBook);
             });
 
         }
     }
 
-    /* Cette classe sert juste a afficher des donnée de test dans le designer */
     public class InDesignDetailsBook : DetailsBook
     {
-        public InDesignDetailsBook() : base(new Book() /*{ Title = "Test Book" }*/) { }
+        public InDesignDetailsBook() : base(new Book() ) { }
     }
 }
 
